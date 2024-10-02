88a72331cbd497a4681f77843d059a36c7bf4e52
dadahon <dadahon@gmail.com>
2024-10-02 21:15:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// DASTURLASHDAN MASALALAR
+// 1-MASALA
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// sum(2, 3);
+// 2-MASALA
+// function toSekunds(minutes) {
+//     console.log(minutes * 60);
+// }
+// toSekunds(3);
+// 3-MASALA
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }
+// nextInteger(-2);
+// 4-MASALA
+// function uchburchakYuzi(asos, balandlik) {
+//     console.log((asos * balandlik) / 2);
+// }
+// uchburchakYuzi(3, 2);
+// 5-MASALA
+// function ageToDays(age) {
+//     console.log(age * 365);
+// }
+
+// ageToDays(65);
+// 6-MASALA
+// function cube(x, y, z) {
+//     console.log(x ** 3, y ** 3, z ** 3);
+// }
+// cube(3, 5, 10);
+// 7-MASALA
+// function firstElement(arr) {
+//     console.log(arr[0]);
+// }
+// firstElement([80, 5, 100]);
+// 8-MASALA
+// function power(x, y) {
+//     console.log(x * y);
+// }
+// power(230, 10);
+// 9-MASALA
+// function hourToSekunds(hour) {
+//     console.log(hour * 3600);
+// }
+// hourToSekunds(2);
+// 10-MASALA
+// function uchinchiTomon(tomon1, tomon2) {
+//     console.log(`Uchinchi tomon: ${tomon1 + tomon2 - 1}`);
+// }
+// uchinchiTomon(8, 10);
