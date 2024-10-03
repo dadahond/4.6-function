1c1062f0d15133c74d17ccacddc0757a6e462f74
dadahon <dadahon@gmail.com>
2024-10-03 10:29:01 +0500

    1-25 done

--- app.js
+++ app.js
@@ -4,27 +4,38 @@
 //     console.log(x + y);
 // }
 // sum(2, 3);
+// sum(-3, -6);
+// sum(7, 3);
 // 2-MASALA
 // function toSekunds(minutes) {
 //     console.log(minutes * 60);
 // }
+// toSekunds(5);
 // toSekunds(3);
+// toSekunds(2);
 // 3-MASALA
 // function nextInteger(int) {
 //     console.log(int + 1);
 // }
-// nextInteger(-2);
+// nextInteger(-0);
+// nextInteger(9);
+// nextInteger(-3);
 // 4-MASALA
 // function uchburchakYuzi(asos, balandlik) {
 //     console.log((asos * balandlik) / 2);
 // }
 // uchburchakYuzi(3, 2);
+// uchburchakYuzi(7, 4);
+// uchburchakYuzi(10, 10);
 // 5-MASALA
 // function ageToDays(age) {
 //     console.log(age * 365);
 // }
 
 // ageToDays(65);
+// ageToDays(0);
+// ageToDays(20);
+
 // 6-MASALA
 // function cube(x, y, z) {
 //     console.log(x ** 3, y ** 3, z ** 3);
@@ -34,19 +45,158 @@
 // function firstElement(arr) {
 //     console.log(arr[0]);
 // }
+// firstElement([1, 2, 3]);
 // firstElement([80, 5, 100]);
 // 8-MASALA
 // function power(x, y) {
 //     console.log(x * y);
 // }
 // power(230, 10);
+// power(110, 3);
+// power(480, 20);
 // 9-MASALA
 // function hourToSekunds(hour) {
 //     console.log(hour * 3600);
 // }
 // hourToSekunds(2);
+// hourToSekunds(10);
+// hourToSekunds(24);
 // 10-MASALA
 // function uchinchiTomon(tomon1, tomon2) {
 //     console.log(`Uchinchi tomon: ${tomon1 + tomon2 - 1}`);
 // }
 // uchinchiTomon(8, 10);
+// uchinchiTomon(5, 7);
+// uchinchiTomon(9, 2);
+// 11-MASALA
+// function qoldiq(son1, son2) {
+//     console.log(son1 % son2);
+// }
+// qoldiq(1, 3);
+// qoldiq(3, 4);
+// qoldiq(-9, 45);
+// 12-MASALA
+// function perimetr(boyi, eni) {
+//     console.log(`To'g'ri to'rtburchakning perimetri ${(boyi + eni) * 2}`);
+// }
+// perimetr(6, 7);
+// perimetr(20, 10);
+// perimetr(2, 9);
+// 13-MASALA
+// function stringQoshish(a) {
+//     console.log("Something" + " " + a);
+// }
+// let a = "is better than nothing";
+// stringQoshish(a);
+// 14-MASALA
+// function kvadrat(a) {
+//     console.log(a ** 2);
+// }
+// kvadrat(5);
+// kvadrat(9);
+// kvadrat(100);
+// 15-MASALA
+// let a = +prompt("son kiriting");
+
+// function noldan(a) {
+//     if (a <= 0) {
+//         console.log("Truth");
+//     } else {
+//         console.log("False");
+//     }
+// }
+// noldan(a);
+// 16-MASALA
+// function ichkiBurchaklar(n) {
+//     console.log(
+//         `${n} burchak ichki burchaklarining yig'indisi ${
+//       (n - 2) * 180
+//     } gradusga teng!`
+//     );
+// }
+// ichkiBurchaklar(3);
+// ichkiBurchaklar(4);
+// ichkiBurchaklar(6);
+// 17-MASALA
+// function ochkolar(x, y) {
+//     console.log(2 * x + 3 * y);
+// }
+// ochkolar(1, 1);
+// ochkolar(7, 5);
+// ochkolar(38, 8);
+// 18-MASALA
+// function nameString(a) {
+//     console.log(a + "Edabit");
+// }
+// nameString("Mubashir");
+// nameString("Matt");
+// nameString("javaScript");
+// 19-MASALA
+// function ikkitaSon(a, b) {
+//     if (a + b < 100) {
+//         console.log("true");
+//     } else {
+//         console.log(`false`);
+//     }
+// }
+// ikkitaSon(22, 15);
+// ikkitaSon(88, 34);
+// ikkitaSon(3, 77);
+// 20-MASALA
+
+// function printArray(n) {
+//     result = [];
+//     for (i = 1; i <= n; i++) {
+//         result.push(i);
+//     }
+//     console.log(result);
+// }
+// printArray(1);
+// printArray(3);
+// printArray(6);
+// 21-MASALA
+// function oyoqlar(tovuq, qoy, sigir) {
+//     console.log(`Jami oyoqlar soni: ${tovuq * 2 + qoy * 4 + sigir * 4} ta`);
+// }
+// oyoqlar(2, 3, 5);
+// oyoqlar(1, 2, 3);
+// oyoqlar(5, 2, 8);
+// 22-MASALA
+// function and(a, b) {
+//     if (a == true && b == false) {
+//         console.log("false");
+//     } else if (a == true && b == true) {
+//         console.log("true");
+//     } else if (a == false && b == true) {
+//         console.log("false");
+//     } else {
+//         console.log("false");
+//     }
+// }
+// and(true, false);
+// and(true, true);
+// and(false, true);
+// and(false, false);
+// 23-MASALA
+// function tengMi(x, y) {
+//     if (x === y) {
+//         console.log(true);
+//     } else console.log(false);
+// }
+// tengMi(4, 8);
+// tengMi(2, 2);
+// tengMi(2, "2");
+// 24-MASALA
+// function futbolOchko(yut, dur, mag) {
+//     console.log(`Ochkolar soni: ${yut * 3 + dur * 1 + mag * 0}`);
+// }
+// futbolOchko(3, 4, 2);
+// futbolOchko(5, 0, 2);
+// futbolOchko(0, 0, 1);
+// 25-MASALA
+// function sekundlar(soat, minut) {
+//     console.log(`${soat * 3600 + minut * 60} sekund`);
+// }
+// sekundlar(1, 3);
+// sekundlar(2, 0);
+// sekundlar(0, 0);
